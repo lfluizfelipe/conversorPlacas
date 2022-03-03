27a48395a33cfb73153f53d13c532f0dae20adbf
Luiz Oliveira <lfluiz.felipelima@gmail.com>
2022-03-03 20:27:08 -0300

    atualizacao codigo

--- conversorPlacas.js
+++ conversorPlacas.js
@@ -1,61 +1,63 @@
 // CONVERSOR DE PLACAS
-function validaPlaca(placa) {
-    const resultado = document.getElementById('actionResult');
+function validatesPlate(plate) {
+    const res = document.getElementById('actionResult');
   
-    let regexPlaca = /^[a-zA-Z]{3}[0-9]{4}$/;
+    let regexPlate = /^[a-zA-Z]{3}[0-9]{4}$/;
   
-    if (regexPlaca.test(placa) != true){
-        resultado.innerHTML = 'Placa inválida, favor digitar novamente!';
+    if (regexPlate.test(plate) != true){
+        res.innerHTML = 'Placa inválida, favor digitar novamente!';
         return false;
         } else {
           return true;
         }
     }
   
-  function convertePlaca(){
-    let placa = document.getElementById('plate').value;
-    const resultado = document.getElementById('actionResult')
-    const isValid = validaPlaca(placa);
+  function convPlate(){
+    let plate = document.getElementById('plate-info').value;
+    const res = document.getElementById('actionResult')
+    const isValid = validatesPlate(plate);
     let convertedPlate = '';
   
     if (!isValid) {
       return;
     }
   
+    let beginPlate = plate.slice(0, 4)
+    let charactersPlate = plate.slice(4, 5)
+    let endPlate = plate.slice(5, 7)
 
-     
-     switch (placa.charAt(4)) {
+
+     switch (charactersPlate) {
      case "0":
-     convertedPlate = placa.replace("0", "A");
+     convertedPlate = charactersPlate.replace("0", "A");
      break;
      case "1":
-     convertedPlate = placa.replace("1", "B");
+     convertedPlate = charactersPlate.replace("1", "B");
      break;
      case "2":
-     convertedPlate = placa.replace("2", "C");
+     convertedPlate = charactersPlate.replace("2", "C");
      break;
      case "3":
-     convertedPlate = placa.replace("3", "D");
+     convertedPlate = charactersPlate.replace("3", "D");
      break;
      case "4":
-     convertedPlate = placa.replace("4", "E");
+     convertedPlate = charactersPlate.replace("4", "E");
      break;
      case "5":
-     convertedPlate = placa.replace("5", "F");
+     convertedPlate = charactersPlate.replace("5", "F");
      break;
      case "6":
-     convertedPlate = placa.replace("6", "G");
+     convertedPlate = charactersPlate.replace("6", "G");
      break;
      case "7":
-     convertedPlate = placa.replace("7", "H");
+     convertedPlate = charactersPlate.replace("7", "H");
      break;
      case "8":
-     convertedPlate = placa.replace("8", "I");
+     convertedPlate = charactersPlate.replace("8", "I");
      break;
      case "9":
-     convertedPlate = placa.replace("9", "J");
+     convertedPlate = charactersPlate.replace("9", "J");
      break;
      }
-     resultado.innerHTML = `Placa Convertida: ${convertedPlate}` 
+     res.innerHTML = `Placa Convertida: ${beginPlate + convertedPlate + endPlate}`
   }
-     
